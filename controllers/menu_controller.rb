77cc39da4e3f1a0adc7221c6ed53847f7de59cf6
Michael Do <mdo3739@gmail.com>
2015-06-03 03:24:17 -0500

    Able to view entry by index

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{@address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View entry by index"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
       main_menu
     when 2
       system "clear"
-      create_entry
+      view_entry
       main_menu
     when 3
       system "clear"
-      search_entries
+      create_entry
       main_menu
     when 4
       system "clear"
-      read_csv
+      search_entries
       main_menu
     when 5
+      system "clear"
+      read_csv
+      main_menu
+    when 6
       puts "Good-bye!"
       exit(0)
     else
@@ -54,6 +59,20 @@ class MenuController
     end
     system "clear"
     puts "End of entries"
+  end
+
+  def view_entry
+    print "Index: "
+    index = (gets.chomp.to_i - 1)
+    entry = @address_book.entries[index]
+    if entry
+      puts entry.to_s
+      entry_submenu(entry)
+      system "clear"
+    else
+      puts "Invalid number"
+      view_entry
+    end
   end
 
   def create_entry
